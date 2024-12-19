da5b984d052aed1a9e4f2f90cb2ec7a7585dddec
DPontaro <n.douzono@gmail.com>
2024-12-19 23:18:09 +0900

    add todos route

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,10 @@ func main() {
         fmt.Fprintf(w, "Hello, Go TODO Backend!")
     })
 
+	http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "TODO List Endpoint")
+	})
+
     fmt.Println("Starting server on :8080")
     if err := http.ListenAndServe(":8080", nil); err != nil {
         panic(err)
