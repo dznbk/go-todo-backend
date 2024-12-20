a8353b40921d9a0aada6bcc65160894cc7fd7ec2
DPontaro <n.douzono@gmail.com>
2024-12-20 22:47:58 +0900

    add go dot env

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
     "fmt"
+	"log"
     "net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintf(w, "Hello, Go TODO Backend!")
     })
@@ -14,8 +28,8 @@ func main() {
 		fmt.Fprintf(w, "TODO List Endpoint")
 	})
 
-    fmt.Println("Starting server on :8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        panic(err)
+	fmt.Printf("Starting server on :%s\n", port)
+    if err := http.ListenAndServe(":"+port, nil); err != nil {
+        log.Fatalf("Server failed: %v", err)
     }
 }
